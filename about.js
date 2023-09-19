9f462ede561db3588dd453e0351f7476dd5956be
redwan-alhajji <139442330+redwan-hajji@users.noreply.github.com>
2023-09-19 14:36:37 +0300

    Add files via upload

---
+++ about.js
@@ -0,0 +1,15 @@
+let menu_icon=document.querySelector("header .container  .fa-bars");
+let nav = document.querySelector("nav");
+let x=document.querySelector("header .container .fa-xmark");
+
+
+menu_icon.addEventListener("click",function(){
+  nav.classList.remove("up_up");
+ nav.classList.add("down_down");
+ nav.classList.add("menu_down_effect");
+})
+x.addEventListener("click",function(){
+ nav.classList.remove("down_down");
+ nav.classList.add("up_up");
+ nav.classList.remove("menu_down_effect"); 
+})
